1e09da757ab768d620f93aca7851f126dddb9cc9
Joshua Wood <JerichoWall@apu.edu>
2019-02-22 09:51:34 -0800

    Finished React refactor, working on filter

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -17,11 +17,11 @@ export default class Content extends Component {
   }
 
   componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({ tabs: tabData, cards: cardData});
   }
 
   changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
+    this.setState({selected: tab});
   };
 
   filterCards = () => {
@@ -41,6 +41,8 @@ export default class Content extends Component {
   };
 
   render() {
+    console.log(this.state.tabs)
+    console.log(this.state.cards)
     return (
       <div className="content-container">
         {/* 
@@ -48,7 +50,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+          tabs={this.state.tabs} 
+          selectTabHandler={this.changeSelected}
+          selectedTab={this.state.selected}  
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
